533e74615054aa25556ecadd990e97b795b1db59
github username <github email address>
2017-05-27 21:02:41 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
 def count_elements(array)
   # code goes here
+  hash = Hash.new(0)
+  arry = array.uniq
+  arry.each do |element|
+    array.each do |element_count|
+      if element == element_count
+        hash[element] += 1
+      end
+    end
+  end
+  hash
 end
- 
